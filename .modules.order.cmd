10dda453a998b76caa832b8c74a6a628fd61ad33
onier <282052309@qq.com>
2021-12-21 11:17:34 +0800

    Delete .modules.order.cmd

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/xuzhenhai/modules.order := {   cat drivers/xuzhenhai/hello/modules.order;   cat drivers/xuzhenhai/kasan-oob/modules.order; :; } | awk '!x[$$0]++' - > drivers/xuzhenhai/modules.order
