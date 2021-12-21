516fe838a6f051493f4a017b1edfd0d15aa29580
onier <282052309@qq.com>
2021-12-21 11:17:24 +0800

    Delete .built-in.a.cmd

--- .built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/xuzhenhai/built-in.a := rm -f drivers/xuzhenhai/built-in.a; ar cDPrST drivers/xuzhenhai/built-in.a drivers/xuzhenhai/hello/built-in.a drivers/xuzhenhai/kasan-oob/built-in.a
